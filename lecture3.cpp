e72d2188042d37734de5eb9b1be3ff7b646c868e
mahesh <maheshbadampudi31@gmail.com>
2023-10-07 17:46:30 +0530

    third lecture

---
+++ lecture3.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a=3;
+    int b=5;
+
+    // a=3;
+    // b=5;
+
+    int c;
+    c=a+b;
+cout<<c<<endl<<"a+b";
+
+cout<<a+b;
+cout<<endl;
+cout<<"a+b";
+
+
+
+
+    return 0;
+}
