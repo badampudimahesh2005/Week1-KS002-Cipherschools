26149d143e9ffe96ac6c69ad1189bd080bd2a41d
mahesh <maheshbadampudi31@gmail.com>
2023-10-08 01:09:57 +0530

    seventh lecture

---
+++ lecture7.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+
+int main(){
+
+    // int password;
+
+    // do{
+    //     cin>>password;
+    // }while(password<999999);
+
+
+
+int count=0;
+    while(1){
+        cout<<"Choclates ";
+        count++;
+
+        if(count>100) break;
+    }
+
+
+int i;
+for(i=0; ; i++){
+    cout<<i<<" ";
+    if(i>100) break;
+    }
+
+
+//     int password;
+//     cout<<"enter the password: ";
+//     cin>>password;
+
+//     while(password<999999){
+//         cout<<"The password does not meet the required condition,please enter the password again"<<endl;
+//         cin>>password;
+//     }
+
+// cout<<"The user has now entered a correct password"<<endl;
+
+return 0;
+}
